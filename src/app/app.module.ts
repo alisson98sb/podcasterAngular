866942634e2054eb69852c6cb152fad4dac987a4
alisson98sb <alisson98sb@gmail.com>
2021-10-06 03:25:55 -0300

    Created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule } from '@angular/router';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { Pagina2Component } from './pages/pagina2/pagina2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Pagina1Component,
+    Pagina2Component,
   ],
   imports: [
     BrowserModule
